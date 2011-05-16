a4eb8a74a7675b0a7cd34e9d94d51d87e2320c19
supertick <supertick@gmail.com>
2011-05-16 12:03:59 +0000

    fixed logging to use RollingFileAppender - removed console logging and console redirect

--- myrobotlab.bat
+++ myrobotlab.bat
@@ -1,1 +1,1 @@
-start javaw -Djava.library.path="./bin"  -cp "myrobotlab.jar;lib/*" org.myrobotlab.service.Invoker -service Invoker services GUIService gui > log.txt
+start javaw -Djava.library.path="./bin"  -cp "myrobotlab.jar;lib/*" org.myrobotlab.service.Invoker -service Invoker services GUIService gui
