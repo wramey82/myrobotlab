a1e430bac41a5bdf77594b8d63f4c3d8b0f67a0e
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2011-06-14 04:41:46 +0000

    chumby updates

--- trunk/src/org/myrobotlab/service/ChumbyBot.java
+++ trunk/src/org/myrobotlab/service/ChumbyBot.java
@@ -10,10 +10,25 @@ public class ChumbyBot extends Service {
 
 	public final static Logger LOG = Logger.getLogger(ChumbyBot.class.getCanonicalName());
 
+	OpenCV camera = new OpenCV("camera");
+	Servo servo = new Servo("pan");
+	Arduino arduino = new Arduino("uBotuino");
+	SensorMonitor sensors = new SensorMonitor("sensors");
+	RemoteAdapter remote = new RemoteAdapter("remote");
+	Speech speech = new Speech("speech");
+
+	transient Thread behavior = null;
+	
 	public abstract class Behavior implements Runnable
 	{
 
 		
+	}
+	
+	public void startBot ()
+	{
+		behavior = new Thread(new ChumbyBot.Explore(),"behavior");
+		behavior.start();
 	}
 	
 	public class Explore extends Behavior
@@ -29,7 +44,43 @@ public class ChumbyBot extends Service {
 			// determine correction
 			// say something relevant e.g. "clear right"
 			// turn appropriate direction 
-			// continue loop/explore			
+			// continue loop/explore
+			
+			try {
+			
+			speech.startService();
+			//speech.cfg.set("isATT", true);
+			speech.speak("I am about to start");
+			remote.startService();
+			camera.startService();
+			arduino.startService();
+			sensors.startService();
+			servo.startService();
+			
+			servo.attach(arduino.name, 12);
+
+			Thread.sleep(10000);
+			
+			while (true)
+			{
+					Thread.sleep(1000);
+					
+					
+					servo.moveTo(10);
+					Thread.sleep(4000);		
+					servo.moveTo(90);
+					Thread.sleep(4000);
+					servo.moveTo(170);
+					Thread.sleep(4000);
+					servo.moveTo(90);
+					
+				
+			}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		
 	}
@@ -48,6 +99,7 @@ public class ChumbyBot extends Service {
 		
 	}
 	
+	
 	@Override
 	public String getToolTip() {
 		return "used as a general template";
@@ -57,29 +109,11 @@ public class ChumbyBot extends Service {
 		org.apache.log4j.BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.DEBUG);
 		
-		/*
+		
 		ChumbyBot chumbybot = new ChumbyBot("chumbybot");
 		chumbybot.startService();
-		*/
-
-		/*
-		OpenCV camera = new OpenCV("camera");
-		camera.startService();
-		camera.capture();
-		*/
-		
-		Arduino arduino = new Arduino("uBotuino");
-		arduino.startService();
-		
-		
-		RemoteAdapter remote = new RemoteAdapter("remote");
-		remote.startService();
-		
-		/*
-		GUIService gui = new GUIService("gui");
-		gui.startService();
-		gui.display();
-		*/
+		chumbybot.startBot();
+				
 	}
 
 
