3f10e210aeb08d4d3134f6bbcb6120370511e5f7
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2011-10-20 23:59:52 +0000

    Jython .. Yay!

--- trunk/src/org/myrobotlab/service/Jython.java
+++ trunk/src/org/myrobotlab/service/Jython.java
@@ -3,7 +3,36 @@ package org.myrobotlab.service;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.myrobotlab.framework.Service;
+import org.python.core.PySystemState;
+import org.python.util.PythonInterpreter;
 
+/**
+ * @author grog
+ *
+ * a Service to access Jython interpreter.  
+ * 
+ * references : 
+ * http://wiki.python.org/jython/InstallationInstructions
+ * http://www.jython.org/javadoc/org/python/util/PythonInterpreter.html
+ * http://etutorials.org/Programming/Python+tutorial/Part+V+Extending+and+Embedding/Chapter+25.+Extending+and+Embedding+Jython/25.2+Embedding+Jython+in+Java/
+ * http://wiki.python.org/moin/PythonEditors - list of editors
+ * http://java-source.net/open-source/scripting-languages
+ * http://java.sun.com/products/jfc/tsc/articles/text/editor_kit/ - syntax highlighting text editor
+ * http://download.oracle.com/javase/tutorial/uiswing/components/generaltext.html#editorkits
+ * http://download.oracle.com/javase/tutorial/uiswing/components/editorpane.html
+ * http://stackoverflow.com/questions/2441525/how-to-use-netbeans-platform-syntax-highlight-with-jeditorpane
+ * http://book.javanb.com/jfc-swing-tutorial-the-a-guide-to-constructing-guis-2nd/ch03lev2sec6.html
+ * 
+ * http://ostermiller.org/syntax/editor.html Text Editor Tutorial - with syntax highlighting
+ * http://stackoverflow.com/questions/4151950/syntax-highlighting-in-jeditorpane-in-java -
+ * example of non-tokenized highlighting
+ * http://saveabend.blogspot.com/2008/06/java-syntax-highlighting-with.html
+ * 
+ * swing components
+ * http://fifesoft.com/rsyntaxtextarea/ <- AMAZING PROJECT
+ * http://www.pushing-pixels.org/2008/06/27/syntax-coloring-for-the-swing-editor-pane.html
+ * 
+ */
 public class Jython extends Service {
 
 	private static final long serialVersionUID = 1L;
@@ -24,12 +53,35 @@ public class Jython extends Service {
 		return "Jython IDE";
 	}
 	
+	PythonInterpreter interp = null;
+	// PyObject interp.eval(String s) - for verifying?
+	
+	public void createPythonInterpreter ()
+	{
+		// TODO - check if exists - destroy / de-initialize if necessary
+		PySystemState.initialize();
+		interp = new PythonInterpreter();		
+	}
+	
+	public void exec (String code)
+	{
+		if (interp == null)
+		{
+			createPythonInterpreter();
+		}
+		
+		interp.exec(code);
+	}
+	
+	
 	public static void main(String[] args) {
 		org.apache.log4j.BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.WARN);
 		
-		Jython template = new Jython("template");
-		template.startService();
+		Jython jython = new Jython("jython");
+		jython.startService();
+		
+		//jython.exec("print \"Hello, World!\" ;");
 		
 		GUIService gui = new GUIService("gui");
 		gui.startService();
