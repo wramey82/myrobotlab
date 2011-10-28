e07a23106cb6974e0b3a1c16bcfa0da1056eb652
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2011-10-28 23:50:19 +0000

    std out & err monitor

---
+++ trunk/src/resource/python/system/monitor.py
@@ -0,0 +1,60 @@
+import sys
+from javax.swing import JFrame
+from javax.swing import JPanel
+from javax.swing import JLabel
+from javax.swing import JTextArea
+from javax.swing import JScrollPane
+from javax.swing import JTabbedPane
+from javax.swing import WindowConstants
+from java.awt import BorderLayout
+from java.lang import Boolean
+
+class Monitor:
+
+  def __init__(self):
+    self.stdout = None
+    self.stderr = None
+    frame = JFrame("Monitor")
+    frame.setSize(400, 300)
+    frame.setLayout(BorderLayout())
+
+    tabPane = JTabbedPane(JTabbedPane.TOP)
+
+    label = JLabel("<html><br>This is a tab1</html>")
+    panel1 = JPanel(BorderLayout())
+    panel1.add(label)
+    log = JTextArea(20, 40)
+    log.setEditable(Boolean("false"))
+    self.log = log
+    panel1.add(JScrollPane(log))
+    tabPane.addTab("monitor", panel1)
+
+    label2 = JLabel("This is a tab2")
+
+    panel2 = JPanel()
+    panel2.add(label2)
+    tabPane.addTab("errors", panel2)
+
+    frame.add(tabPane)
+    #frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    frame.setVisible(True)
+
+  def write(self,string):
+    self.log.append(string)
+    
+  def attach(self):
+    if (self.stdout == None):    
+      self.stdout = sys.stdout
+      self.stderr = sys.stderr
+      sys.stdout = self
+      sys.stderr = self
+   
+  def detach(self):
+    if (self.stdout != None):
+      sys.stdout = self.stdout
+      sys.stderr = self.stderr
+      self.stdout = None
+      self.stderr = None
+    
+monitor = Monitor()
+monitor.attach()
