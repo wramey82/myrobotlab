c2d0d504824ec42609c9b93350afeb3cf89cfaa0
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2011-12-07 14:22:30 +0000

    build and script clean up

--- trunk/myrobotlab.bat
+++ trunk/myrobotlab.bat
@@ -1,1 +1,1 @@
-start javaw -Djava.library.path="./bin"  -cp "myrobotlab.jar;lib/*" org.myrobotlab.service.Invoker -service Invoker services GUIService gui
+start javaw -Djava.library.path="./bin"  -cp "libraries/jar/*" org.myrobotlab.service.ServiceFactory -service services ServiceFactory gui GUIService 
