7a267949b40ffe766eb67cdda0fe88da0c2649ff
SwedaKonsult <SwedaKonsult@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-06-17 15:44:39 +0000

    Fixed parsing of the enums so that it can be done properly based on the expected strings coming from other systems (e.g., GUI)

--- myrobotlab/src/org/myrobotlab/logging/LogAppender.java
+++ myrobotlab/src/org/myrobotlab/logging/LogAppender.java
@@ -31,5 +31,57 @@ public enum LogAppender {
 		public String toString() {
 			return "remote";
 		}
+	};
+	
+	/**
+	 * Safely parse the input parameter. Default: None.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static LogAppender tryParse(String str) {
+		if (str == null || str.length() < 4) {
+			return None;
+		}
+		if (str.equals(Console.toString())) {
+			return Console;
+		} else if (str.equals(ConsoleGui.toString())) {
+			return ConsoleGui;
+		} else if (str.equals(File.toString())) {
+			return File;
+		} else if (str.equals(Remote.toString())) {
+			return Remote;
+		} else {
+			return None;
+		}
+	}
+	
+	/**
+	 * Safely parse the input parameter. Default: None.
+	 * 
+	 * @param str
+	 * @return
+	 * @throws IllegalArgumentException if there is no match
+	 */
+	public static LogAppender parse(String str) {
+		if (str == null || str.length() < 4) {
+			throw new IllegalArgumentException();
+		}
+		if (str.equals(Console.toString())) {
+			return Console;
+		}
+		if (str.equals(ConsoleGui.toString())) {
+			return ConsoleGui;
+		}
+		if (str.equals(File.toString())) {
+			return File;
+		}
+		if (str.equals(Remote.toString())) {
+			return Remote;
+		}
+		if (str.equals(None.toString())){
+			return None;
+		}
+		throw new IllegalArgumentException(str);
 	}
 }
--- myrobotlab/src/org/myrobotlab/logging/LogLevel.java
+++ myrobotlab/src/org/myrobotlab/logging/LogLevel.java
@@ -17,7 +17,7 @@ public enum LogLevel {
 			return "INFO";
 		}
 	},
-	Warning {
+	Warn {
 		public String toString() {
 			return "WARN";
 		}
@@ -31,5 +31,43 @@ public enum LogLevel {
 		public String toString() {
 			return "FATAL";
 		}
+	};
+	
+	/**
+	 * Safely parse the input parameter. Default: Info.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static LogLevel tryParse(String str) {
+		if (str == null || str.length() < 4) {
+			return LogLevel.Info;
+		}
+		StringBuilder formattedString = new StringBuilder(str.length())
+			.append(Character.toUpperCase(str.charAt(0)))
+			.append(str.substring(1).toLowerCase());
+		try {
+			return LogLevel.valueOf(formattedString.toString());
+		} catch (IllegalArgumentException ex) {
+			return LogLevel.Info;
+		}
+	}
+	
+	/**
+	 * Parse the input parameter.
+	 * 
+	 * @param str
+	 * @return
+	 * @throws IllegalArgumentException if there is no match
+	 */
+	public static LogLevel parse(String str) {
+		if (str == null || str.length() < 4) {
+			throw new IllegalArgumentException(str);
+		}
+		// change the string to pascal casing
+		StringBuilder formattedString = new StringBuilder(str.length())
+			.append(Character.toUpperCase(str.charAt(0)))
+			.append(str.substring(1).toLowerCase());
+		return LogLevel.valueOf(formattedString.toString());
 	}
 }
