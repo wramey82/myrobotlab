36cb58cee27b4041887090689987485d11dce93f
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-06-23 13:54:08 +0000

    build related changes

--- trunk/myrobotlab/thirdParty/build/resource/root/myrobotlab.bat
+++ trunk/myrobotlab/thirdParty/build/resource/root/myrobotlab.bat
@@ -5,4 +5,4 @@ REM -Dhttp.proxyHost=webproxy -Dhttp.proxyPort=8080 -Dhttp.proxyUserName="myuser
 REM start javaw starts java without another shell window on windows
 REM to display system out messages enable logging or run simply as java ..<parameters>..
 set PATH=%PATH%;%CD%\libraries\native\x86.32.windows;%CD%\libraries\native\x86.64.windows
-start javaw -Djava.library.path="libraries/native/x86.32.windows;libraries/native/x86.64.windows"  -cp "libraries/jar/*;" org.myrobotlab.service.Runtime -service gui GUIService -logLevel DEBUG 
+start javaw -Djava.library.path="libraries/native/x86.32.windows;libraries/native/x86.64.windows"  -cp "libraries/jar/*;" org.myrobotlab.service.Runtime -service gui GUIService jython Jython -logLevel DEBUG 
