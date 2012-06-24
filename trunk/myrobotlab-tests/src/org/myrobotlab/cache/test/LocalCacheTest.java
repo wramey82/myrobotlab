2775460b98dac5ab5cd3e21563e115241b081e5b
SwedaKonsult <SwedaKonsult@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-06-24 17:21:33 +0000

    Changed Jython commandMap from HashMap to HashSet since we're only using the key.

--- trunk/myrobotlab-tests/src/org/myrobotlab/cache/test/LocalCacheTest.java
+++ trunk/myrobotlab-tests/src/org/myrobotlab/cache/test/LocalCacheTest.java
@@ -1,6 +1,9 @@
 package org.myrobotlab.cache.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.ConcurrentMap;
 
