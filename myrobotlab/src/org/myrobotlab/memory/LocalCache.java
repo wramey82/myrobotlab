2be3050f99e332610e99a7366fb022105637b07b
SwedaKonsult <SwedaKonsult@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-06-24 23:56:19 +0000

    Implemented initial framework for CacheManager. Added the notion of timeouts for cached items. Created additional interface ManagedCache so that the CacheManager has additional functionality available to it, which is not available to the rest of the code that simply uses the Cache interface.

--- myrobotlab/src/org/myrobotlab/memory/LocalCache.java
+++ myrobotlab/src/org/myrobotlab/memory/LocalCache.java
@@ -25,7 +25,11 @@ public class LocalCache extends BaseCache {
 	/**
 	 * The cache of this instance.
 	 */
-	private final ConcurrentMap<String, Object> cache;
+	private final ConcurrentMap<String, Object> items;
+	private final ConcurrentMap<String, Long> itemTimeouts;
+	private final long timeout;
+	private long nextTimeout;
+	private final boolean useTimeout;
 	
 	/**
 	 * Constructor.
@@ -37,16 +41,22 @@ public class LocalCache extends BaseCache {
      * @param concurrencyLevel the estimated number of concurrently
      * updating threads. The implementation performs internal sizing
      * to try to accommodate this many threads.
+     * @param the amount of time in ms after which an item should time out.
      * @throws IllegalArgumentException if the initial capacity is
      * negative or the load factor or concurrencyLevel are
      * non-positive.
 	 */
-	public LocalCache(int initialSize, float loadFactor, int concurrencyLevel) {
-		cache = new ConcurrentHashMap<String, Object>(initialSize, loadFactor, concurrencyLevel);
+	public LocalCache(int initialSize, float loadFactor, int concurrencyLevel, int timeout) {
+		items = new ConcurrentHashMap<String, Object>(initialSize, loadFactor, concurrencyLevel);
+		itemTimeouts = new ConcurrentHashMap<String, Long>(initialSize, loadFactor, concurrencyLevel);
+		this.timeout = timeout;
+		nextTimeout = 0l;
+		useTimeout = this.timeout > 0l;
 	}
 
     /**
      * Constructor. Default load factor (0.75) and concurrencyLevel (16).
+     * Default timeout: 0 (never).
      *
      * @param initialCapacity the initial capacity. The implementation
      * performs internal sizing to accommodate this many elements.
@@ -54,26 +64,72 @@ public class LocalCache extends BaseCache {
      * elements is negative.
      */
     public LocalCache(int initialCapacity) {
-        this(initialCapacity, DEFAULT_LOAD_FACTOR, DEFAULT_CONCURRENCY_LEVEL);
+        this(initialCapacity, DEFAULT_LOAD_FACTOR, DEFAULT_CONCURRENCY_LEVEL, 0);
     }
 
 	@Override
 	protected void addToCache(String name, Object value) {
-		cache.put(name, value);
+		items.put(name, value);
+		// only set a timeout for the item if we want to timeout things in this cache
+		if (timeout > 0l) {
+			itemTimeouts.put(name, System.currentTimeMillis() + timeout);
+		}
+	}
+
+	@Override
+	protected void clearCache() {
+		items.clear();
+		// only need to clear things out if they've been used
+		if (useTimeout) {
+			itemTimeouts.clear();
+			nextTimeout = 0l;
+		}
 	}
 
 	@Override
 	protected boolean contains(String name) {
-		return cache.containsKey(name);
+		return items.containsKey(name);
+	}
+
+	@Override
+	protected void expireItem(String name) {
+		if (name == null || name.isEmpty() || !itemTimeouts.containsKey(name) || !useTimeout) {
+			return;
+		}
+		// time it out
+		itemTimeouts.put(name, 0l);
+		// make sure it's clear that something needs to be timed out
+		nextTimeout = 0l;
 	}
 
 	@Override
 	protected Object getFromCache(String name) {
-		return cache.get(name);
+		if (!items.containsKey(name)) {
+			return null;
+		}
+		return items.get(name);
 	}
 
 	@Override
 	protected void removeFromCache(String name) {
-		cache.remove(name);
+		if (!items.containsKey(name)) {
+			return;
+		}
+		// TODO is this needed in order to make sure that the handle is removed?
+		items.put(name, null);
+		items.remove(name);
+		// only remove it if we're using timeouts
+		if (useTimeout) {
+			itemTimeouts.remove(name);
+		}
+	}
+
+	@Override
+	protected void timeoutCache() {
+		if (!useTimeout || (nextTimeout > 0l && nextTimeout > System.currentTimeMillis())) {
+			// nothing to time out right now
+			return;
+		}
+		// TODO loop through itemTimeouts in order to see if one of them needs to be timed out
 	}
 }
