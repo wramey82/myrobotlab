b7fe9522c346128335c6cda07a765d952f27fdc1
SwedaKonsult <SwedaKonsult@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-06-24 17:21:42 +0000

    Cleaned up some of the code regarding transforming object types into primitives.

---
+++ myrobotlab-tests/src/org/myrobotlab/reflection/test/TestInstantiator.java
@@ -0,0 +1,53 @@
+package org.myrobotlab.reflection.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.myrobotlab.reflection.Instantiator;
+
+public class TestInstantiator {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.DEBUG);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNewInstanceStringObjectArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetNewInstanceClassOfQObjectArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testInvokeMethod_Double() {
+		Object object = 0d;
+		String method = "parseDouble";
+		Object[] params = {"1.34433"};
+		Instantiator.invokeMethod(object, method, params);
+	}
+
+}
--- myrobotlab/src/org/myrobotlab/cache/BaseCache.java
+++ myrobotlab/src/org/myrobotlab/cache/BaseCache.java
@@ -3,29 +3,18 @@
  */
 package org.myrobotlab.cache;
 
-import java.util.HashSet;
+import org.myrobotlab.reflection.Instantiator;
 
 /**
  * @author SwedaKonsult
  *
  */
 public abstract class BaseCache implements Cache {
-
-	/**
-	 * Allow for checking if a boxed primitive is being used.
-	 */
-	private final static HashSet<Class<?>> primitiveTypes = new HashSet<Class<?>>();
 	
-	static {
-	    primitiveTypes.add(Boolean.class);
-	    primitiveTypes.add(Character.class);
-	    primitiveTypes.add(Byte.class);
-	    primitiveTypes.add(Short.class);
-	    primitiveTypes.add(Integer.class);
-	    primitiveTypes.add(Long.class);
-	    primitiveTypes.add(Float.class);
-	    primitiveTypes.add(Double.class);
-	}
+	private static final boolean DEFAULT_BOOL = false;
+	private static final byte DEFAULT_BYTE = 0;
+	private static final int DEFAULT_INT = 0;
+	private static final short DEFAULT_SHORT = 0;
 
 	/**
 	 * Internal method for BaseCache to actually add items to the implementing cache.
@@ -72,39 +61,17 @@ public abstract class BaseCache implements Cache {
 	@SuppressWarnings("unchecked")
 	public <T> T get(String name, Class<? extends T> cls) {
 		if (name == null || !contains(name)) {
-			if (!primitiveTypes.contains(cls)) {
+			if (!Instantiator.isPrimitive(cls)) {
 				return null;
 			}
-			// else return whichever primitive they're asking for
-			if (cls.isAssignableFrom(Integer.class)) {
-				return (T) new Integer(0);
-			}
-			if (cls.isAssignableFrom(Byte.class)) {
-				return (T) new Byte(Byte.MIN_VALUE);
-			}
-			if (cls.isAssignableFrom(Short.class)) {
-				return (T) new Short(Short.MIN_VALUE);
-			}
-			if (cls.isAssignableFrom(Double.class)) {
-				return (T) new Double(0d);
-			}
-			if (cls.isAssignableFrom(Float.class)) {
-				return (T) new Float(0f);
-			}
-			if (cls.isAssignableFrom(Long.class)) {
-				return (T) new Long(0l);
-			}
-			if (cls.isAssignableFrom(Boolean.class)) {
-				return (T) Boolean.FALSE;
-			}
-			return (T) new Character('\u0000');
+			return (T) Instantiator.getPrimitive(cls);
 		}
 		Object value = getFromCache(name);
 
 		if (value != null && cls.isInstance(value)) {
 			return (T) value;
 		}
-		if (!primitiveTypes.contains(cls)) {
+		if (!Instantiator.isPrimitive(cls)) {
 			return null;
 		}
 		// else return whichever primitive they're asking for
@@ -133,6 +100,134 @@ public abstract class BaseCache implements Cache {
 	}
 	
 	/**
+	 * Get a boolean primitive value from the cache.
+	 * Tests for: Boolean, Integer, Byte, Short, String (parseBoolean)
+	 * 
+	 * @param name
+	 * @return false if nothing is found or the cached value is not a boolean value
+	 */
+	public boolean getBool(String name) {
+		if (name == null || !contains(name)) {
+			return DEFAULT_BOOL;
+		}
+		Object value = getFromCache(name);
+		if (value ==  null) {
+			return DEFAULT_BOOL;
+		}
+		if (value instanceof Boolean) {
+			boolean b = (Boolean) value;
+			return b;
+		}
+		if (value instanceof Integer) {
+			int i = (Integer) value;
+			return i == 0;
+		}
+		if (value instanceof Byte) {
+			byte b = (Byte) value;
+			return b == 0;
+		}
+		if (value instanceof Short) {
+			short s = (Short) value;
+			return s == 0;
+		}
+		if (!(value instanceof String)) {
+			return DEFAULT_BOOL;
+		}
+		return parseWithDefault((String) value, DEFAULT_BOOL);
+	}
+	
+	/**
+	 * Get an byte primitive value from the cache.
+	 * Tests for: Byte, Short, String (parseByte)
+	 * 
+	 * @param name
+	 * @return 0 if nothing is found or the cached value was not an byte value
+	 */
+	public byte getByte(String name) {
+		if (name == null || !contains(name)) {
+			return DEFAULT_BYTE;
+		}
+		Object value = getFromCache(name);
+		if (value ==  null) {
+			return DEFAULT_BYTE;
+		}
+		if (value instanceof Byte) {
+			byte b = (Byte) value;
+			return b;
+		}
+		if (value instanceof Short) {
+			short s = (Short) value;
+			return (byte) s;
+		}
+		if (!(value instanceof String)) {
+			return DEFAULT_BYTE;
+		}
+		return parseWithDefault((String) value, DEFAULT_BYTE);
+	}
+	
+	/**
+	 * Get an int primitive value from the cache.
+	 * Tests for: Integer, Byte, Short, String (parseInt)
+	 * 
+	 * @param name
+	 * @return 0 if nothing is found or the cached value was not an integer value
+	 */
+	public int getInt(String name) {
+		if (name == null || !contains(name)) {
+			return DEFAULT_INT;
+		}
+		Object value = getFromCache(name);
+		if (value ==  null) {
+			return DEFAULT_INT;
+		}
+		if (value instanceof Integer) {
+			int i = (Integer) value;
+			return (int) i;
+		}
+		if (value instanceof Byte) {
+			byte b = (Byte) value;
+			return (int) b;
+		}
+		if (value instanceof Short) {
+			short s = (Short) value;
+			return (int) s;
+		}
+		if (!(value instanceof String)) {
+			return DEFAULT_INT;
+		}
+		return parseWithDefault((String) value, DEFAULT_INT);
+	}
+	
+	/**
+	 * Get an short primitive value from the cache.
+	 * Tests for: Short, Byte, String (parseShort)
+	 * 
+	 * @param name
+	 * @return 0 if nothing is found or the cached value was not an integer value
+	 */
+	public short getShort(String name) {
+		if (name == null || !contains(name)) {
+			return DEFAULT_SHORT;
+		}
+		Object value = getFromCache(name);
+		if (value ==  null) {
+			return DEFAULT_SHORT;
+		}
+		if (value instanceof Short) {
+			short s = (Short) value;
+			return s;
+		}
+		if (value instanceof Byte) {
+			byte b = (Byte) value;
+			return (short) b;
+		}
+		if (!(value instanceof String)) {
+			return DEFAULT_SHORT;
+		}
+		return parseWithDefault((String) value, DEFAULT_SHORT);
+	}
+	
+	/**
 	 * Add a value to the cache.
 	 * 
 	 * @param name cannot be null or empty
@@ -143,5 +238,58 @@ public abstract class BaseCache implements Cache {
 			return;
 		}
 		addToCache(name, value);
+	}
+	
+	/**
+	 * Try to parse a boolean.
+	 * 
+	 * @param value
+	 * @param defaultBool return value if the string cannot be parsed into a boolean
+	 * @return
+	 */
+	private boolean parseWithDefault(String value, boolean defaultBool) {
+		return Boolean.parseBoolean(value);
+	}
+	
+	/**
+	 * Try to parse a byte.
+	 * 
+	 * @param value
+	 * @param defaultByte return value if the string cannot be parsed into an byte
+	 * @return
+	 */
+	private byte parseWithDefault(String value, byte defaultByte) {
+		try {
+			return Byte.parseByte(value);
+		} catch (NumberFormatException e) {}
+		return defaultByte;
+	}
+	
+	/**
+	 * Try to parse an integer.
+	 * 
+	 * @param value
+	 * @param defaultInt return value if the string cannot be parsed into an integer
+	 * @return
+	 */
+	private int parseWithDefault(String value, int defaultInt) {
+		try {
+			return Integer.parseInt(value);
+		} catch (NumberFormatException e) {}
+		return defaultInt;
+	}
+	
+	/**
+	 * Try to parse a short.
+	 * 
+	 * @param value
+	 * @param defaultShort return value if the string cannot be parsed into an short
+	 * @return
+	 */
+	private short parseWithDefault(String value, short defaultShort) {
+		try {
+			return Short.parseShort(value);
+		} catch (NumberFormatException e) {}
+		return defaultShort;
 	}
 }
