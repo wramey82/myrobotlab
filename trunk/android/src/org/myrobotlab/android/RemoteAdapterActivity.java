963fab60e9eded139cb23ed2682e745576704b40
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2012-07-25 11:21:57 +0000

    compensate for crappy dependency problems of android adt update

--- trunk/android/src/org/myrobotlab/android/RemoteAdapterActivity.java
+++ trunk/android/src/org/myrobotlab/android/RemoteAdapterActivity.java
@@ -1,5 +1,8 @@
 package org.myrobotlab.android; 
 
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 import org.myrobotlab.service.RemoteAdapter;
 
 import android.os.Bundle;
@@ -13,6 +16,16 @@ public class RemoteAdapterActivity extends ServiceActivity {
         super.onCreate(savedInstanceState, R.layout.remoteadapter);
         myService = (RemoteAdapter)sw.service;   
         setText(R.id.udpdata, myService.UDPPort);
+        ArrayList<InetAddress> addresses = RemoteAdapter.getLocalAddresses();
+        StringBuffer ips = new StringBuffer();
+        for (int i = 0; i < addresses.size(); ++i)
+        {
+        	ips.append(addresses.get(i).getHostAddress());
+        	ips.append(";");
+        }
+        
+        setText(R.id.local_addresses, ips.toString() );
+        
     }
 
 	@Override
