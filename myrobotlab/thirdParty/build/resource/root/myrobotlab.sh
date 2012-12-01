78a41659690617ec2a66644cce2f54e815cf055f
supertick <supertick@gmail.com>
2012-12-01 15:46:10 +0000

    tweaked scripts

--- myrobotlab/thirdParty/build/resource/root/myrobotlab.sh
+++ myrobotlab/thirdParty/build/resource/root/myrobotlab.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-CLASSPATH=.; export CLASSPATH=$CLASSPATH$(find "$PWD/libraries/jar" -name '*.jar' -type f -printf ':%p\n' | sort -u | tr -d '\n'); echo $CLASSPATH
-
 # Mac's don't use LD_LIBRARY_PATH yet its 
 # required to load shared objects on Linux systems
 LD_LIBRARY_PATH=`pwd`/libraries/native/arm.32.linux:`pwd`/libraries/native/x86.32.linux:`pwd`/libraries/native/x86.64.linux:${LD_LIBRARY_PATH}
@@ -13,5 +11,5 @@ export LD_LIBRARY_PATH
 # LD_LIBRARY_PATH needed by Linux systems
 # -Djava.library.path= needed by mac
 
-java -classpath "./libraries/jar/*" -Djava.library.path="./libraries/native/arm.32.linux:./libraries/native/x86.32.linux:./libraries/native/x86.64.linux:./libraries/native/x86.32.mac" org.myrobotlab.service.Runtime -service gui GUIService python Python 
+java -classpath "./libraries/jar/*:./libraries/jar/x86.32.linux/*:./libraries/jar/x86.64.linux/*:" -Djava.library.path="./libraries/native/arm.32.linux:./libraries/native/x86.32.linux:./libraries/native/x86.64.linux:./libraries/native/x86.32.mac" org.myrobotlab.service.Runtime -service gui GUIService python Python 
 
