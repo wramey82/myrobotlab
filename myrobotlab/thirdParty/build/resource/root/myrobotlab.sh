52c0359ecef2fc9f577cb2ed85f0e3a3a4f95a67
supertick <supertick@gmail.com>
2013-01-01 17:09:23 +0000

    added - DYLD_LIBRARY_PATH for mac in the myrobotlab.sh

--- myrobotlab/thirdParty/build/resource/root/myrobotlab.sh
+++ myrobotlab/thirdParty/build/resource/root/myrobotlab.sh
@@ -5,6 +5,9 @@
 LD_LIBRARY_PATH=`pwd`/libraries/native/arm.32.linux:`pwd`/libraries/native/x86.32.linux:`pwd`/libraries/native/x86.64.linux:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH
 
+DYLD_LIBRARY_PATH=`pwd`/libraries/native/x86.32.mac:`pwd`/libraries/native/x86.64.mac:${DYLD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH
+
 # java -d32  force 32 bit 
 # -classpath ":myrobotlab.jar:./lib/*"  - make note : on Linux ; on Windows ! 
 # -Djava.library.path=./bin - can not change or modify LD_LIBRARY_PATH after jvm starts 
