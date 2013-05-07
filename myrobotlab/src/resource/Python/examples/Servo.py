01c7f83d5ea59dc26a7119bbfd4416a7644194ac
supertick <supertick@gmail.com>
2013-05-07 23:50:25 +0000

    Servo.py update

--- myrobotlab/src/resource/Python/examples/Servo.py
+++ myrobotlab/src/resource/Python/examples/Servo.py
@@ -18,38 +18,33 @@ arduino.setSerialPortParams(115200, 8, 1, 0)
 # attach servo
 servo01.attach("arduino", 9)
 
-#servo02 = Runtime.create("servo02","Servo")
-#servo02.startService()
-
 # fast sweep
 servo01.moveTo(179)
 sleep(0.5)
-#servo02.moveTo(179)
-#sleep(0.5)
+
 servo01.moveTo(10)
 sleep(0.5)
-#servo02.moveTo(10)
-#sleep(0.5)
+
 servo01.moveTo(179)
 sleep(0.5)
-#servo02.moveTo(179)
-#sleep(0.5)
+
 servo01.moveTo(10)
 sleep(0.5)
-#servo02.moveTo(10)
-#sleep(0.5)
+
 servo01.moveTo(179)
 sleep(0.5)
-#servo02.moveTo(179)
-#sleep(0.5)
+
 servo01.moveTo(10)
 sleep(0.5)
-#servo02.moveTo(10)
-#sleep(0.5)
 
 # slow sweep
 #for x in range(10,170):
 #  servo01.moveTo(x)
 #  sleep(0.05)
   
+servo01.setSpeed(0.99) # set speed to 99% of full speed
+servo01.moveTo(90) 
+sleep(0.5)
+servo01.setSpeed(0.25) # set speed to 25% of full speed
+servo01.moveTo(180)
 
