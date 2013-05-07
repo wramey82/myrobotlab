d386bd0a0bee6c9e9dd287c84df2c1b01bfcb68c
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-05-07 23:21:26 +0000

    added comment

--- trunk/myrobotlab/src/resource/Python/examples/Cron.py
+++ trunk/myrobotlab/src/resource/Python/examples/Cron.py
@@ -1,3 +1,5 @@
+# The following script will send a quote to a Log and Speech service every minute.
+
 speech = Runtime.createAndStart("speech","Speech")
 cron   =  Runtime.createAndStart("cron", "Cron")
 log   =  Runtime.createAndStart("log", "Log")
