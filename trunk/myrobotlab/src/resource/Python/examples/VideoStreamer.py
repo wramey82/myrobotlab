99747982acf3778df3b7df5bda5528edbc86b2de
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-05-07 19:55:54 +0000

    Beginning of the Python Library !!!!

---
+++ trunk/myrobotlab/src/resource/Python/examples/VideoStreamer.py
@@ -0,0 +1,19 @@
+from org.myrobotlab.service import Runtime
+from org.myrobotlab.service import OpenCV
+from org.myrobotlab.service import VideoStreamer
+
+# create a video source (opencv) & a video streamer
+opencv = Runtime.createAndStart("opencv","OpenCV")
+streamer = Runtime.createAndStart("streamer","VideoStreamer")
+
+# attache them
+streamer.attach(opencv)
+
+# add a pyramid down filter and gray to minimize the data
+opencv.addFilter("pyramidDown", "PyramidDown");
+opencv.addFilter("gray", "Gray");
+
+# start the camera
+opencv.capture();
+
+# go to http://localhost:9090/output
