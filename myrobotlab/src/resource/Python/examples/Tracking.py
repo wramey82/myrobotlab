169270eeacc7c8dc94975382b8ac4ccc83ce8ead
supertick <supertick@gmail.com>
2013-05-08 04:31:38 +0000

    tracking script safety updates

--- myrobotlab/src/resource/Python/examples/Tracking.py
+++ myrobotlab/src/resource/Python/examples/Tracking.py
@@ -6,6 +6,14 @@
 tracker = Runtime.createAndStart("tracker","Tracking")
 tracker.setRestPosition(90, 90)
 tracker.setSerialPort("COM12")
+
+# setXMinMax & setYMinMax (min, max) - this will set the min and maximum
+# x value it will send the servo - typically this is not needed
+# because the tracking service will pull the min and max positions from 
+# the servos it attaches too
+tracker.setXMinMax(10, 170)
+tracker.setYMinMax(10, 170)
+
 # setServoPins (x, y) set the servo of the pan and tilt repectively
 tracker.setServoPins(13,12)
 # tracker.setCameraIndex(1) #change cameras if necessary
