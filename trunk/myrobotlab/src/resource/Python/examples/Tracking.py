7e9b197e2cbf3ba1cfa512a34eb53997a7caa9ab
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-05-08 03:55:12 +0000

    initial tracking script

---
+++ trunk/myrobotlab/src/resource/Python/examples/Tracking.py
@@ -0,0 +1,14 @@
+# a minimal tracking script - this will start all peer 
+# services and attach everything appropriately
+# change parameters depending on your pan tilt, pins and
+# Arduino details
+
+tracker = Runtime.createAndStart("tracker","Tracking")
+tracker.setRestPosition(90, 90)
+tracker.setSerialPort("COM12")
+tracker.setServoPins(13,12)
+# tracker.setCameraIndex(1) #change cameras if necessary
+ 
+tracker.trackLKPoint()
+
+#tracker.learnBackground()
