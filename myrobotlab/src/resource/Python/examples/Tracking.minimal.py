60e8546fcfb581a996297cbe775a4a395042299a
supertick <supertick@gmail.com>
2013-05-16 18:11:59 +0000

    Tracking updates - no sticky - pid can be set manually - setMinMax actually works - new pid invert & direct methods

--- myrobotlab/src/resource/Python/examples/Tracking.minimal.py
+++ myrobotlab/src/resource/Python/examples/Tracking.minimal.py
@@ -1,7 +1,12 @@
 # a minimal tracking script - this will start all peer# services and attach everything appropriately# change parameters depending on your pan tilt, pins and# Arduino detailstracker = Runtime.create("tracker","Tracking")
-# minimal configuration# set rest at x = 90 y = 90tracker.setRestPosition(90, 90)# set serial port of the Arduinotracker.setSerialPort("COM12")# setServoPins (x, y) set the servo of the pan and tilt repectivelytracker.setServoPins(13,12)
-# setXMinMax & setYMinMax (min, max) - this will set the min and maximum# x value it will send the servo - typically this is not needed# because the tracking service will pull the min and max positions from # the servos it attaches tootracker.setXMinMax(10, 170)tracker.setYMinMax(10, 170)
+# setXMinMax & setYMinMax (min, max) - this will set the min and maximum
+# x value it will send the servo - typically this is not needed
+# because the tracking service will pull the min and max positions from 
+# the servos it attaches too
+tracker.setXMinMax(10, 170)
+tracker.setYMinMax(10, 170)
 
+# minimal configuration# set rest at x = 90 y = 90tracker.setRestPosition(90, 90)# set serial port of the Arduinotracker.setSerialPort("COM12")# setServoPins (x, y) set the servo of the pan and tilt repectivelytracker.setServoPins(13,12)
 #change cameras if necessary
 # tracker.setCameraIndex(1)
 # start the tracking servicetracker.startService()# put it in LK track modetracker.trackLKPoint()
--- myrobotlab/src/resource/Python/examples/Tracking.safe.py
+++ myrobotlab/src/resource/Python/examples/Tracking.safe.py
@@ -5,18 +5,35 @@
 # create a tracking service
 tracker = Runtime.create("tracker","Tracking")
 
-# create servos BEFORE starting the tracking service
-# so we can specify values for the servos and specify names
-# before it starts tracking
-# additionally all the other services can be created, configured
-# and manually attached if desired
+# we can create all the peer services for tracker manually
+# giving them new names and different values
+# then we can attach them - this must be done before
+# starting the tracking service
 
 rotation = Runtime.create("rotation","Servo")
 neck = Runtime.create("neck","Servo")
 arduino = Runtime.create("arduino","Arduino")
+xpid = Runtime.create("xpid","PID");
+ypid = Runtime.create("ypid","PID");
+
+# adjust values
 arduino.setSerialDevice("COM12")
 eye = Runtime.create("eye","OpenCV")
 eye.setCameraIndex(1)
+
+# flip the pid if needed
+xpid.invert()
+xpid.setOutputRange(-3, 3)
+xpid.setPID(10.0, 0, 1.0)
+xpid.setControllerDirection(1)
+xpid.setSetpoint(0.5) # we want the target in the middle of the x
+
+# flip the pid if needed
+# ypid.invert()
+ypid.setOutputRange(-3, 3)
+ypid.setPID(10.0, 0, 1.0)
+ypid.invert()
+ypid.setSetpoint(0.5)
 
 # set safety limits - servos
 # will not go beyond these limits
@@ -26,27 +43,15 @@ rotation.setPositionMax(170)
 neck.setPositionMin(50)
 neck.setPositionMax(170)
 
-# here we are binding are new servos with different names
-# to the tracking service.  If not specified the tracking service
-# will create a servo named x and y
-
-# setServoPins (x, y) set the servo of the pan and tilt repectively
+# here we are attaching to non-default
+# manually created peer services
 
 tracker.attach(arduino)
 tracker.attachServos(rotation, 13, neck, 12)
 tracker.attach(eye)
+tracker.attach(xpid, ypid)
 
 tracker.setRestPosition(90, 90)
-
-# setXMinMax & setYMinMax (min, max) - this will set the min and maximum
-# x value it will send the servo - typically this is not needed
-# because the tracking service will pull the min and max positions from 
-# the servos it attaches too
-tracker.setXMinMax(60, 170)
-tracker.setYMinMax(60, 170)
-
-#change cameras if necessary
-# tracker.setCameraIndex(1) 
 
 tracker.startService()
 tracker.trackLKPoint()
