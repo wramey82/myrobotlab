7c684a4d628cc121c3c7e05d9ae901704288a56e
supertick <supertick@gmail.com>
2013-05-20 21:03:17 +0000

    minimal InMoov script

---
+++ myrobotlab/src/resource/Python/examples/InMoov.minimal.py
@@ -0,0 +1,34 @@
+# a very minimal script for InMoov
+
+inMoov = Runtime.createAndStart("inMoov", "InMoov")
+
+# attach an arduino to InMoov
+# possible board types include uno atmega168 atmega328p atmega2560 atmega1280 atmega32u4
+# the MRLComm.ino sketch must be loaded into the Arduino for MyRobotLab control !
+inMoov.attachArduino("right","uno","COM12")
+inMoov.attachHand("right")
+
+# system check
+inMoov.systemCheck()
+
+# listen for these key words
+inMoov.startListening("rest | open hand | close hand | manual | voice control")
+
+# voice control
+def heard():
+  data = msg_ear_recognized.data[0]
+  print "heard ", data
+  #mouth.setLanguage("fr")
+  
+  mouth.speak("you said " + data)
+  
+  if (data == "rest"):
+    inMoov.rest() 
+  elif (data == "open hand"):
+    inMoov.handOpen("right")
+  elif (data == "close hand"):
+    inMoov.handClose("right")
+  elif (data == "manual"):
+    inMoov.lockOutAllGrammarExcept("voice control")
+  elif (data == "voice control"):
+    inMoov.clearGrammarLock()
