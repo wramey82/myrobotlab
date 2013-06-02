02df4c6a6a2295bd7d9032eb0b1f90b1a00096ff
supertick <supertick@gmail.com>
2013-06-02 15:40:39 +0000

    com port updated

--- myrobotlab/src/resource/Python/examples/InMoov.head.py
+++ myrobotlab/src/resource/Python/examples/InMoov.head.py
@@ -5,7 +5,7 @@ inMoov = Runtime.createAndStart("inMoov", "InMoov")
 # possible board types include uno atmega168 atmega328p atmega2560 atmega1280 atmega32u4
 # the MRLComm.ino sketch must be loaded into the Arduino for MyRobotLab control !
 # set COM number according to the com of your Arduino board
-inMoov.attachArduino("left","uno","COM12")
+inMoov.attachArduino("left","uno","COM7")
 # if you have a laptop with a camera the one in InMoov is likely to be index #1
 inMoov.setCameraIndex(1)
 inMoov.attachHead("left")
