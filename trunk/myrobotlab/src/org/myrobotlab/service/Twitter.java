50100d1136fa69505937f160fa569928b5c83461
alessandro.didonna@gmail.com <alessandro.didonna@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-06 23:12:30 +0000

    Twitter.java added

---
+++ trunk/myrobotlab/src/org/myrobotlab/service/Twitter.java
@@ -0,0 +1,55 @@
+package org.myrobotlab.service;
+
+import org.myrobotlab.framework.Service;
+import org.myrobotlab.logging.Level;
+import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.logging.LoggingFactory;
+import org.myrobotlab.memory.Memory;
+import org.myrobotlab.memory.MemoryChangeListener;
+import org.myrobotlab.memory.Node;
+import org.slf4j.Logger;
+
+
+public class Twitter extends Service {
+
+	private static final long serialVersionUID = 1L;
+
+	public final static Logger log = LoggerFactory.getLogger(Twitter.class.getCanonicalName());
+	
+	public Twitter(String n) {
+		super(n, Twitter.class.getCanonicalName());	
+	}
+
+	@Override
+	public String getToolTip() {
+		return "used as a general template";
+	}
+
+	@Override 
+	public void stopService()
+	{
+		super.stopService();
+	}
+	
+	@Override
+	public void releaseService()
+	{
+		super.releaseService();
+	}
+
+	public static void main(String[] args) {
+		LoggingFactory.getInstance().configure();
+		LoggingFactory.getInstance().setLevel(Level.WARN);
+
+		Twitter template = new Twitter("template");
+		template.startService();			
+		
+		Runtime.createAndStart("gui", "GUIService");
+		/*
+		 * GUIService gui = new GUIService("gui"); gui.startService();
+		 * gui.display();
+		 */
+	}
+
+
+}
