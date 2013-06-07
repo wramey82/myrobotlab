4d7e9337f6a43ffb79830cc327853dd258ad36c5
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-07 20:11:33 +0000

    updated

--- trunk/myrobotlab/src/resource/Python/examples/Twitter.uploadFromOpenCV.py
+++ trunk/myrobotlab/src/resource/Python/examples/Twitter.uploadFromOpenCV.py
@@ -1,7 +1,18 @@
 twitter = Runtime.createAndStart("twitter","Twitter")
+opencv = Runtime.createAndStart("opencv","OpenCV")
+
+# add a filter to display
+# opencv must be a running service, with a camera capturing!!!!
+opencv.addFilter("PyramidDown")
+opencv.capture()
+
 # replace security information with your own keys : 
-#register your application at https://dev.twitter.com/ and obtain your own keys
+# register your application at https://dev.twitter.com/ and obtain your own keys
 twitter.setSecurity("yourConsumerKey","yourConsumerSecret", "yourAccessToken", "yourAccessTokenSecret")
 twitter.configure()
-#opencv must be a running service, with a camera capturing!!!!
+
+# sleep - let camera initialize and
+# video processor threads start etc... say CHEESE !
+sleep(5)
+
 twitter.uploadImage(opencv.getDisplay() , "text to upload");
