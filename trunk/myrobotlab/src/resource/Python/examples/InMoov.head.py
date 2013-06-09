eb36cfa35148e0976ac29006c04ded3f60b84076
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-09 16:17:30 +0000

    inmoov.head.py updates

--- trunk/myrobotlab/src/resource/Python/examples/InMoov.head.py
+++ trunk/myrobotlab/src/resource/Python/examples/InMoov.head.py
@@ -1,4 +1,4 @@
-# a larger script for InMoov
+# a head script for InMoov
 inMoov = Runtime.createAndStart("inMoov", "InMoov")
 
 # variables to adjust
@@ -9,34 +9,27 @@ cameraIndex = 1
 # possible board types include uno atmega168 atmega328p atmega2560 atmega1280 atmega32u4
 # the MRLComm.ino sketch must be loaded into the Arduino for MyRobotLab control !
 # set COM number according to the com of your Arduino board
-
 inMoov.attachArduino("left","uno", leftSerialPort)
-# if you have a laptop with a camera the one in InMoov is likely to be index #1
-inMoov.setCameraIndex(cameraIndex)
 inMoov.attachHead("left")
-inMoov.trackPoint(0.5,0.5)
 
 # system check
 inMoov.systemCheck()
+
+# if you have a laptop with a camera the one in InMoov is likely to be index #1
+inMoov.setCameraIndex(cameraIndex)
  
 # listen for these key words
 # to get voice to work - you must be attached to the internet for
 # at least the first time
-inMoov.startListening("track | freeze track | manual | voice control")
- 
-# voice control
-def heard():
-  data = msg_ear_recognized.data[0]
-  print "heard ", data
+ear = inMoov.getEar()
+ear.addCommand("rest", inMoov.getName(), "rest")
+ear.addCommand("track", inMoov.getName(), "track")
+ear.addCommand("freeze track", inMoov.getName(), "clearTrackingPoints")
+ear.addCommand("capture gesture", inMoov.getName(), "captureGesture")
+ear.addCommand("manual", ear.getName(), "lockOutAllGrammarExcept", "voice control")
+ear.addCommand("voice control", ear.getName(), "clearLock")
 
-  mouth.speak("you said " + data)
-  if (data == "rest"):
-    inMoov.rest() 
-  elif (data == "track"):
-    inMoov.trackPoint(0.5,0.5)
-  elif (data == "freeze track"):
-    inMoov.clearTrackingPoints();
-  elif (data == "manual"):
-    inMoov.lockOutAllGrammarExcept("voice control")
-  elif (data == "voice control"):
-    inMoov.clearGrammarLock()
+ear.addComfirmations("yes","correct","right","yeah","ya")
+ear.addNegations("no","incorrect","wrong","nope","nah")
+
+ear.startListening() 
