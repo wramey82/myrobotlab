1d61a208255bc22b7aeb900959720a20bb30c07a
paulklemstine@yahoo.com <paulklemstine@yahoo.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-14 21:34:40 +0000

    blah..I'm in SVN hell!

--- trunk/myrobotlab/src/org/myrobotlab/service/GUIDynamic.java
+++ trunk/myrobotlab/src/org/myrobotlab/service/GUIDynamic.java
@@ -1,14 +1,13 @@
 package org.myrobotlab.service;
 
-import java.awt.Component;
-
-import org.myrobotlab.DynamicGUI.Desktop;
 import org.myrobotlab.control.ServiceGUI;
 import org.myrobotlab.framework.ServiceWrapper;
 import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.logging.LoggingFactory;
 import org.slf4j.Logger;
+
+import dynamicGUI.Desktop;
 
 
 
