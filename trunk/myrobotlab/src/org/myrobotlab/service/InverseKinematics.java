e09f03096e1af19574fc4dda55d7266d85298c84
alessandro.didonna@gmail.com <alessandro.didonna@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-20 01:43:20 +0000

    InverseKinematics update-new engine(3d point- base rotation and angles)

--- trunk/myrobotlab/src/org/myrobotlab/service/InverseKinematics.java
+++ trunk/myrobotlab/src/org/myrobotlab/service/InverseKinematics.java
@@ -5,25 +5,11 @@ import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.logging.LoggingFactory;
 import org.slf4j.Logger;
-
+import org.myrobotlab.inverseKinematics.*;
 
 public class InverseKinematics extends Service {
 
-	public double x;
-	public double y;
-	public double z;
-	public double l1;
-	public double l2;
-	public double x2;
-	public double z2;
-	public double l12;
-	public double l22;
-	public double form;
-	public double teta1;
-    public double form2;
-    public double form3;
-    public double teta2;
-    public double teta3;
+	protected IKEngine ikEngine;
 	
 	private static final long serialVersionUID = 1L;
 
@@ -38,60 +24,35 @@ public class InverseKinematics extends Service {
 		return "used as a general template";
 	}
 	
-	public void setCoordinates(double a,double b,double c){
-	     x = a;
-	     y = b;
-	     z = c;
-	}
-	public void setLengths(double d,double e){
-		 l1 = d;
-		 l2 = e;
-	}
-	public void computeAngles (){
-		 x2 = Math.pow(x,2);
-		 z2 = Math.pow(z,2);
-		 l12 = Math.pow(l1,2);
-		 l22 = Math.pow(l2,2);
-		 form = ((x2+z2+l12-l22)/(2*l1*l2));
-		 teta1 = (Math.acos(form)+Math.atan2(z,x));
-		 form2 = (z-l1*Math.sin(teta1)); 
-		 form3 = (x-l1*Math.cos(teta1));
-		 teta1 = Math.round(Math.toDegrees(teta1));
-		 teta2 = Math.round(Math.toDegrees(Math.atan2(form2, form3)));
-		 teta3 = Math.round(Math.toDegrees(Math.atan2(y,x)));
-		 
-	}
-	
-	public double getTeta1(){
-		
-		return teta1;
-		
-	}
-	
-    public double getTeta2(){
-		
-		return teta2;
-		
-	}
-    
-    public double getTeta3(){
-		
-		return teta3;
-		
-	}
+	 public void setDOF(int dof){
+		    ikEngine= new IKEngine(dof);
+		    ikEngine.setMode(dof);
+	 }
+	 
+	 public void setStructure(int nlink, double length){
+		    ikEngine.setLinkLength(nlink,length);
+	 }
+	 
+	 public void compute(){
+		    ikEngine.calculate();
+		    ikEngine.getPosition();
+	 }
+	 
+	 public void setPoint(double x,double y, double z){
+		    ikEngine.setGoal(x,y,z);
+	 }
 
 	public static void main(String[] args) {
 		LoggingFactory.getInstance().configure();
 		LoggingFactory.getInstance().setLevel(Level.WARN);
 
 		InverseKinematics inversekinematics = new InverseKinematics("inversekinematics");
-		inversekinematics.startService();
-		inversekinematics.setCoordinates(50,50,0);
-		inversekinematics.setLengths(100,100);
-		inversekinematics.computeAngles();
-		System.out.println("First rod angle is :" + inversekinematics.getTeta1() );
-		System.out.println("Second rod angle is " + inversekinematics.getTeta2() );
-		System.out.println("Base rotation angle is :" + inversekinematics.getTeta3() );
+		inversekinematics.setDOF(3);
+		inversekinematics.setStructure(0,100);
+		inversekinematics.setStructure(1,100);
+		inversekinematics.setStructure(2,100);
+		inversekinematics.setPoint(200,200,200);
+		inversekinematics.compute();
 		
 		Runtime.createAndStart("gui", "GUIService");
 		/*
