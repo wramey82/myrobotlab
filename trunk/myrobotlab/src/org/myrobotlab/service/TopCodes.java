05527d8328a04ed86fc02c99414a01ebc05964fb
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-27 14:00:00 +0000

    topcodes service update

--- trunk/myrobotlab/src/org/myrobotlab/service/TopCodes.java
+++ trunk/myrobotlab/src/org/myrobotlab/service/TopCodes.java
@@ -1,11 +1,16 @@
 package org.myrobotlab.service;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.logging.Logging;
 import org.myrobotlab.logging.LoggingFactory;
 import org.slf4j.Logger;
 
@@ -18,7 +23,7 @@ public class TopCodes extends Service {
 	private static final long serialVersionUID = 1L;
 
 	public final static Logger log = LoggerFactory.getLogger(TopCodes.class.getCanonicalName());
-	Scanner scanner;
+	Scanner scanner = new Scanner();
 	
 	public TopCodes(String n) {
 		super(n, TopCodes.class.getCanonicalName());	
@@ -29,28 +34,24 @@ public class TopCodes extends Service {
 		return "used as a general topcodes";
 	}
 
-	@Override 
-	public void stopService()
-	{
-		super.stopService();
-	}
-	
-	@Override
-	public void releaseService()
-	{
-		super.releaseService();
-	}
-
-	@Override
-	public void startService()
-	{
-		super.releaseService();
-		scanner = new Scanner();
-	}
 	
 	List<TopCode> scan(BufferedImage img)
 	{
 		return scanner.scan(img);
+	}
+
+	List<TopCode> scan(String filename)
+	{
+		try {
+			BufferedImage img;
+			img = ImageIO.read(new File(filename));
+			return scanner.scan(img);
+		} catch (IOException e) {
+			error(e.getMessage());
+			Logging.logException(e);
+		}
+		
+		return null;
 	}
 
 	
@@ -60,6 +61,7 @@ public class TopCodes extends Service {
 
 		TopCodes topcodes = new TopCodes("topcodes");
 		topcodes.startService();			
+		topcodes.scan("somepicture.jpg");
 		
 		Runtime.createAndStart("gui", "GUIService");
 		/*
