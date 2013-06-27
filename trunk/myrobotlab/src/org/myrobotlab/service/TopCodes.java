4b0113636c58d4cbc71bfa296f026deff05de8dd
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-06-27 13:48:58 +0000

    moved topcodes to the repo

--- trunk/myrobotlab/src/org/myrobotlab/service/TopCodes.java
+++ trunk/myrobotlab/src/org/myrobotlab/service/TopCodes.java
@@ -1,10 +1,16 @@
 package org.myrobotlab.service;
 
+import java.awt.image.BufferedImage;
+import java.util.List;
+
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.logging.LoggingFactory;
 import org.slf4j.Logger;
+
+import topcodes.Scanner;
+import topcodes.TopCode;
 
 
 public class TopCodes extends Service {
@@ -12,6 +18,7 @@ public class TopCodes extends Service {
 	private static final long serialVersionUID = 1L;
 
 	public final static Logger log = LoggerFactory.getLogger(TopCodes.class.getCanonicalName());
+	Scanner scanner;
 	
 	public TopCodes(String n) {
 		super(n, TopCodes.class.getCanonicalName());	
@@ -19,7 +26,7 @@ public class TopCodes extends Service {
 
 	@Override
 	public String getToolTip() {
-		return "used as a general template";
+		return "used as a general topcodes";
 	}
 
 	@Override 
@@ -34,12 +41,25 @@ public class TopCodes extends Service {
 		super.releaseService();
 	}
 
+	@Override
+	public void startService()
+	{
+		super.releaseService();
+		scanner = new Scanner();
+	}
+	
+	List<TopCode> scan(BufferedImage img)
+	{
+		return scanner.scan(img);
+	}
+
+	
 	public static void main(String[] args) {
 		LoggingFactory.getInstance().configure();
 		LoggingFactory.getInstance().setLevel(Level.WARN);
 
-		TopCodes template = new TopCodes("template");
-		template.startService();			
+		TopCodes topcodes = new TopCodes("topcodes");
+		topcodes.startService();			
 		
 		Runtime.createAndStart("gui", "GUIService");
 		/*
