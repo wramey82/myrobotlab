3a104763825765ae58996dafab1b73600a92d1ae
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-07-14 16:12:06 +0000

    more webgui

--- trunk/myrobotlab/src/org/myrobotlab/webgui/RESTProcessor.java
+++ trunk/myrobotlab/src/org/myrobotlab/webgui/RESTProcessor.java
@@ -1,113 +1,171 @@
 package org.myrobotlab.webgui;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Properties;
 
 import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.logging.Logging;
 import org.myrobotlab.service.interfaces.HTTPProcessor;
 import org.myrobotlab.service.interfaces.ServiceInterface;
 import org.myrobotlab.webgui.NanoHTTPD.Response;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 import org.slf4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
 
 public class RESTProcessor implements HTTPProcessor {
 
 	public final static Logger log = LoggerFactory.getLogger(RESTProcessor.class.getCanonicalName());
 
 	private HashSet<String> uris = new HashSet<String>();
-	
+
+	transient private Serializer serializer = new Persister();
+
 	@Override
 	public Response serve(String uri, String method, Properties header, Properties parms, Socket socket) {
-		// TODO top level is return format /html /text /soap /xml /gson /json a default could exist - start with SOAP response
+
+		String returnFormat = "gson";
+
+		// TODO top level is return format /html /text /soap /xml /gson /json a
+		// default could exist - start with SOAP response
 		// default is not specified but adds {/rest/xml} /services ...
 		// TODO - custom display /displays
 		// TODO - structured rest fault responses
-		
-		// handle escape character \ - I know this is pretty "anti"- rest but in a binding between input
-		// parameters there has to be some form of escape character
-		
-		//String[] keys = uri.split("(?<!\\\\)/");
-		//String[] keys = uri.split("(?<!/)/");
-		//String[] keys = uri.split("/");
-		
-		
-		// scan for first /
-		// find the next / 
-		// 	if (next != /) - split
-		//	scan forward - if the set of ////  is odd split at beginning and remove - so next split is even
-		
-		// this fails when a parameter ends with / :P - it will incorrectly put the / at the beginning of the next
-		// parameter
-		
-		/*
-		ArrayList<String> result = new ArrayList<String>();
-		
-		int pos0 = 0;
-		int pos1 = uri.indexOf("/");
-		boolean isOdd = false;
-		
-		while (pos1 != -1)
-		{
-			result.add(uri.substring(pos0, pos1));
-			pos0 = pos1;
-			pos1 = uri.indexOf("/", pos1 + 1);
-		}
-		*/
-		
+
 		String[] keys = uri.split("/");
-		
+
 		// decode everything
-		for (int i = 0; i < keys.length; ++i)
-		{
+		for (int i = 0; i < keys.length; ++i) {
 			keys[i] = decodePercent(keys[i], true);
 		}
-		
-		if ("/services".equals(uri))
-		{
+
+		if ("/services".equals(uri)) {
 			// get runtime list
 			log.info("services request");
 			REST rest = new REST();
 			String services = rest.getServices();
-			
+
 			Response response = new Response("200 OK", "text/html", services);
-			
 			return response;
-		} else if (keys.length == 3){
-			log.info("here");
-			// get a specific service instance - return STATE !
-		} else if (keys.length > 3){
-			// get a specific service instance - execute method --with parameters--
+
+		} else if (keys.length > 3) {
+			// get a specific service instance - execute method --with
+			// parameters--
 			String serviceName = keys[2];
 			String fn = keys[3];
 			Object[] typedParameters = null;
 
-			
 			ServiceInterface si = org.myrobotlab.service.Runtime.getService(serviceName);
-			
+
 			// get parms
-			if (keys.length > 4)
-			{
+			if (keys.length > 4) {
 				// copy paramater part of rest uri
 				String[] stringParams = new String[keys.length - 4];
-				for (int i = 0; i < keys.length - 4; ++i)
-				{
+				for (int i = 0; i < keys.length - 4; ++i) {
 					stringParams[i] = keys[i + 4];
 				}
-				
-				TypeConverter.getInstance(); // FIXME - make better singleton - or make threadsafe - new'd
+
+				TypeConverter.getInstance();
 				typedParameters = TypeConverter.getTypedParams(si.getClass(), fn, stringParams);
 			}
-			
-			// TODO - handle return type - 
-			// TODO top level is return format /html /text /soap /xml /gson /json /base16 a default could exist - start with SOAP response
+
+			// TODO - handle return type -
+			// TODO top level is return format /html /text /soap /xml /gson
+			// /json /base16 a default could exist - start with SOAP response
 			Object returnObject = si.invoke(fn, typedParameters);
-			
+			String xml = null;
+
+			// Message msg = new Message();
+			// FIXME FIXME FIXME - put in a ResponseHanlder object with simple
+			// imputs/outputs String InputStream / OutputStream !!!!
+			if ("xml".equals(returnFormat)) {
+
+				if (returnObject != null) {
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					// if (returnObject)
+					try {
+
+						ReturnType returnType = new ReturnType();
+
+						// FIXME - handle
+						if (returnObject.getClass() == ArrayList.class) {
+							returnType.arrayList = (ArrayList<Object>) returnObject;
+						} else if (returnObject.getClass() == Array.class) {
+							returnType.array = (Object[]) returnObject;
+						} else if (returnObject.getClass() == HashMap.class) {
+							returnType.map = (HashMap<String, Object>) returnObject;
+						} else {
+							returnType.returnObject = returnObject;
+						}
+
+						serializer.write(returnType, out);
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block SOAP FAULT ??
+						e.printStackTrace();
+					}
+					xml = String.format("<%sResponse>%s</%sResponse>", fn, new String(out.toByteArray()), fn);
+				} else {
+					xml = String.format("<%sResponse />", fn);
+				}
+
+				Response response = new Response("200 OK", "text/xml", xml);
+				return response;
+
+			} else if ("gson".equals(returnFormat)) {
+				ByteArrayOutputStream out = null;
+				
+				// FIXME - a "response" of some sort is important - even if the returned object is null
+				// the signal which you get from a event can be significant - the quetion arrises
+				// gson is to encode the returned data - but if there is no data - it still
+				// should return ... "blank" ?
+				
+				String encodedResponse = "null"; // Hah .. dorky yet appropriate?
+				
+				try {
+
+					if (returnObject != null) {
+						Gson gson = new Gson(); // FIXME - threadsafe? singeton?
+						out = new ByteArrayOutputStream();
+						JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+						writer.setIndent("  ");
+
+						writer.beginArray();
+						gson.toJson(returnObject, returnObject.getClass(), writer);
+						// for (Message message : messages) {
+						// gson.toJson(message, Message.class, writer);
+						// }
+						writer.endArray();
+						writer.close();
+						
+						encodedResponse = new String(out.toByteArray());
+						
+					}
+				} catch (Exception e) {
+					Logging.logException(e);
+				}
+				
+				Response response = new Response("200 OK", "text/text", encodedResponse);
+				
+				return response;
+			}
+
 			// handle response depending on type
-			// TODO - make structured !!! 
+			// TODO - make structured !!!
 			// Right now just return string object
-			Response response = new Response("200 OK", "text/html", (returnObject == null)?"":returnObject.toString());
-			return response; 
+			// Response response = new Response("200 OK", "text/xml",
+			// (returnObject == null)?String.format("<%sResponse></%sResponse>",
+			// method, method):returnObject.toString());
+			Response response = new Response("200 OK", "text/xml", (returnObject == null) ? String.format("<%sResponse></%sResponse>", method, method) : xml);
+			return response;
 		}
 		return null;
 	}
@@ -116,43 +174,40 @@ public class RESTProcessor implements HTTPProcessor {
 	public HashSet<String> getURIs() {
 		return uris;
 	}
-	
-	public void addURI(String uri)
-	{
+
+	public void addURI(String uri) {
 		uris.add(uri);
 	}
-	
+
 	/**
 	 * Decodes the percent encoding scheme. <br/>
 	 * For example: "an+example%20string" -> "an example string"
 	 */
-	private String decodePercent(String str, boolean decodeForwardSlash)  {
-		
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < str.length(); i++) {
-				char c = str.charAt(i);
-				switch (c) {
-				case '+':
-					sb.append(' ');
-					break;
-				case '%':
-					if ("2F".equalsIgnoreCase(str.substring(i + 1, i + 3)) && !decodeForwardSlash)
-					{
-						log.info("found encoded / - leaving");
-						sb.append("%2F");
-					} else {
-						sb.append((char) Integer.parseInt(str.substring(i + 1, i + 3), 16));
-					}
-					i += 2;
-					break;
-				default:
-					sb.append(c);
-					break;
-				}
-			}
-			return new String(sb.toString().getBytes());
-	
-	}
+	private String decodePercent(String str, boolean decodeForwardSlash) {
 
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			switch (c) {
+			case '+':
+				sb.append(' ');
+				break;
+			case '%':
+				if ("2F".equalsIgnoreCase(str.substring(i + 1, i + 3)) && !decodeForwardSlash) {
+					log.info("found encoded / - leaving");
+					sb.append("%2F");
+				} else {
+					sb.append((char) Integer.parseInt(str.substring(i + 1, i + 3), 16));
+				}
+				i += 2;
+				break;
+			default:
+				sb.append(c);
+				break;
+			}
+		}
+		return new String(sb.toString().getBytes());
+
+	}
 
 }
