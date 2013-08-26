838c725418c907fb7f0a9dbaacb81eacc5ca1546
alessandro.didonna@gmail.com <alessandro.didonna@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-08-26 13:47:54 +0000

    Arduino.Servo.dumbBot.py update

--- trunk/myrobotlab/src/resource/Python/examples/Arduino.Servo.dumbBot.py
+++ trunk/myrobotlab/src/resource/Python/examples/Arduino.Servo.dumbBot.py
@@ -1,4 +1,5 @@
 arduino = Runtime.createAndStart("arduino","Arduino")
+joy= runtime.createAndStart("joy","Joystick")
 dx 	= Runtime.createAndStart("dx","Servo")
 sx	= Runtime.createAndStart("sx","Servo")
 arduino.setSerialDevice("COM3", 57600, 8, 1, 0)
@@ -26,12 +27,28 @@ def stop ():
  sx.moveTo(90)
  return
 
-turnR()
-sleep(2)
-turnL()
-sleep(2)
-forward()
-sleep(2)
-back()
-sleep(2)
+def x():
+    x = msg_joy_XAxisRaw.data[0]
+    print x
+    if (x == 1):
+     turnR()
+    elif (x == -1):
+     turnL()
+    else :
+     stop()
+    return
+def y():
+    y = msg_joy_YAxisRaw.data[0]
+    print y
+    if (y == -1):
+     forward()
+    elif (y == 1):
+     back()
+    else :
+     stop()
+    return 
+#create a message route from joy to python so we can listen for button
+joy.addListener("XAxisRaw", python.name, "x")
+joy.addListener("YAxisRaw", python.name, "y")
+
 stop()
