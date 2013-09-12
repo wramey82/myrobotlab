abefff1868b44e7e7681109c5faf025d4f40faf5
supertick <supertick@gmail.com>
2013-09-12 19:45:06 +0000

    Serial publishing method is publishByte not read - fixed in scripts

--- myrobotlab/src/resource/Python/examples/Serial.asciiToChar.py
+++ myrobotlab/src/resource/Python/examples/Serial.asciiToChar.py
@@ -4,11 +4,11 @@ import time
 #create a Serial service named serial
 serial = Runtime.createAndStart("serial","Serial")
 def input():
- code = msg_serial_read.data[0]
+ code = msg_serial_publishByte.data[0]
  decoded = ''.join(chr(code))
  print decoded
 #have python listening to serial
-serial.addListener("read", python.name, "input") 
+serial.addListener("publishByte", python.name, "input") 
 #connect to a serial port COM4 57600 bitrate 8 data bits 1 stop bit 0 parity
 serial.connect("COM9", 57600, 8, 1, 0)
 #sometimes its important to wait a little for hardware to get ready
