39166d08ed414b41fc7c10014bb8c2a949c1cb73
supertick <supertick@gmail.com>
2013-09-24 00:35:14 +0000

    refactored script to go along with new tracking service

--- myrobotlab/src/resource/Python/examples/Tracking.minimal.py
+++ myrobotlab/src/resource/Python/examples/Tracking.minimal.py
@@ -1,15 +1,4 @@
-# a minimal tracking script - this will start all peer# services and attach everything appropriately# change parameters depending on your pan tilt, pins and# Arduino detailstracker = Runtime.create("tracker","Tracking")
-# setXMinMax & setYMinMax (min, max)
-# this will set the min and maximum
-# values of the servos 
-tracker.setXMinMax(10, 170)
-tracker.setYMinMax(10, 170)
-
-# set rest x,ytracker.setRestPosition(90, 90)# set serial port of the Arduinotracker.setSerialPort("COM12")# se the pan and tilt pins
-tracker.setServoPins(13,12)
-#change cameras if necessary
-tracker.setCameraIndex(1)
-# start the tracking servicetracker.startService()
+# a minimal tracking script - this will start all peer# services and attach everything appropriately# change parameters depending on your pan tilt, pins and# Arduino details# all commented code is not necessary but allows custom# optionsport = "COM12"xServoPin = 3;yServoPin = 6;tracker = Runtime.create("tracker", "Tracking")tracker.connect(port)tracker.attachServos(xServoPin, yServoPin)# set limits if necessary# default is servo limits# tracker.setServoLimits(0, 180, 0, 180) # set rest position default is 90 90# tracker.setRestPosition(90, 90) tracker.setPIDDefaults()tracker.startService()
 # set a point and track it# there are two interfaces one is float value
 # where 0.5,0.5 is middle of screen
 tracker.trackPoint(0.5, 0.5)
