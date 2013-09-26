9f864eeca943de13155b192ad464d4b47a5b0918
supertick <supertick@gmail.com>
2013-09-26 16:31:43 +0000

    servo fix and gps service added

---
+++ myrobotlab/src/org/myrobotlab/service/GPS.java
@@ -0,0 +1,52 @@
+package org.myrobotlab.service;
+
+import org.myrobotlab.framework.Service;
+import org.myrobotlab.logging.Level;
+import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.logging.LoggingFactory;
+import org.slf4j.Logger;
+
+
+public class GPS extends Service {
+
+	private static final long serialVersionUID = 1L;
+
+	public final static Logger log = LoggerFactory.getLogger(GPS.class.getCanonicalName());
+	
+	public GPS(String n) {
+		super(n, GPS.class.getCanonicalName());	
+	}
+
+	@Override
+	public String getToolTip() {
+		return "used as a general template";
+	}
+
+	@Override 
+	public void stopService()
+	{
+		super.stopService();
+	}
+	
+	@Override
+	public void releaseService()
+	{
+		super.releaseService();
+	}
+
+	public static void main(String[] args) {
+		LoggingFactory.getInstance().configure();
+		LoggingFactory.getInstance().setLevel(Level.WARN);
+
+		GPS template = new GPS("template");
+		template.startService();			
+		
+		Runtime.createAndStart("gui", "GUIService");
+		/*
+		 * GUIService gui = new GUIService("gui"); gui.startService();
+		 * gui.display();
+		 */
+	}
+
+
+}
