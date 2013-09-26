b9a2de99254799344d8db511c0751a6ee9da8998
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-09-26 14:10:57 +0000

    script update

--- trunk/myrobotlab/src/resource/Python/examples/InMoov.Alessandruino.py
+++ trunk/myrobotlab/src/resource/Python/examples/InMoov.Alessandruino.py
@@ -1,16 +1,24 @@
 # a minimal tracking script - this will start all peer
 # services and attach everything appropriately
-# change parameters depending on your pan tilt, pins and
+# change parameters depending on your pan eyeY, pins and
 # Arduino details
 # all commented code is not necessary but allows custom
 # options
  
 port = "COM8"
-xServoPin = 3;
-yServoPin = 6;
+eyeXPin = 3
+eyeYPin = 6
+headXPin = 10
+headYPin = 9 
  
- 
-tracker = Runtime.create("tracker", "Tracking")
+eyes = Runtime.create("eyes", "Tracking")
+head = Runtime.create("head", "Tracking")
+
+# name to bind correctly
+eyes.xName = "eyeX";
+eyes.yName = "eyeY";
+eyes.xpidName = "eyeXPID"
+eyes.ypidName = "eyeYPID"
  
 # naming - binding of peer services is done with service names
 # the Tracking service will use the following default names
@@ -18,41 +26,39 @@ tracker = Runtime.create("tracker", "Tracking")
 # xpidName = "xpid" - the PID service to control X tracking
 # ypidName = "ypid" - the PID service to control Y tracking
 # xName = "x" - the x servo (pan)
-# yName = "y" - the y servo (tilt)
+# yName = "y" - the y servo (eyeY)
 # opencvName = "opencv" - the camera
  
 # after the Tracking service is "created" you may create peer service
 # and change values of that service - for example if we want to invert a
 # servo :
-tilt = Runtime.create("tilt", "Servo")
-tilt.setInverted(True)
-# now we bind it to the Tracking service by changing the name of Tracking's xName :
-tracker.yName = "tilt"
-# this must be done before tracker.startService() is called
+eyeY = Runtime.create("eyeY", "Servo")
+eyeY.setInverted(True)
+
  
 # initialization 
-tracker.connect(port)
-tracker.attachServos(xServoPin, yServoPin)
+eyes.connect(port)
+eyes.attachServos(eyeXPin, eyeYPin)
  
 # set limits if necessary
 # default is servo limits
-tracker.setServoLimits(65, 90, 22, 85) 
+eyes.setServoLimits(65, 90, 22, 85) 
  
 # set rest position default is 90 90
-tracker.setRestPosition(80, 47) 
+eyes.setRestPosition(80, 47) 
  
-#tracker.setPIDDefaults()
+#eyes.setPIDDefaults()
 # changing PID values 
 # setXPID(Kp, Ki, Kd, Direction 0=direct 1=reverse, Mode 0=manual 1= automatic, minOutput, maxOutput, sampleTime, setPoint);
 # defaults look like this_AUTOMATIC
-tracker.setXPID(10.0, 1, 0.1, 0, 1, -10, 10, 30, 0.5)
-tracker.setYPID(10.0, 1, 0.1, 0, 1, -10, 10, 30, 0.5)
-tracker.startService()
+eyes.setXPID(10.0, 1, 0.1, 0, 1, -10, 10, 30, 0.5)
+eyes.setYPID(10.0, 1, 0.1, 0, 1, -10, 10, 30, 0.5)
+eyes.startService()
  
 # set a point and track it
 # there are two interfaces one is float value
 # where 0.5,0.5 is middle of screen
-tracker.trackPoint(0.5, 0.5)
+# eyes.trackPoint(0.5, 0.5)
  
 # don't be surprised if the point does not
 # stay - it needs / wants a corner in the image
@@ -61,4 +67,4 @@ tracker.trackPoint(0.5, 0.5)
 # opencv screen
  
 # face tracking from face detection filter
-tracker.faceDetect()
+eyes.faceDetect()
