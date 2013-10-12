8a27df82ce11c232345ae2dac4ea1665838e2f46
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2013-10-12 14:55:16 +0000

    plantoid updates

--- trunk/myrobotlab/src/org/myrobotlab/service/Plantoid.java
+++ trunk/myrobotlab/src/org/myrobotlab/service/Plantoid.java
@@ -13,13 +13,19 @@ public class Plantoid extends Service {
 	private static final long serialVersionUID = 1L;
 
 	// peer services
-	transient public Servo d3, d4, d5, d2, pan, tilt;
+	/**
+	 * Leg servos and pan tilt kit
+	 */
+	transient public Servo leg1, leg2, leg3, leg4, pan, tilt;
 	transient public Arduino arduino;
 	// video0 = rgbpilot cam
 	// video1 = pink plant static NIR - 
 	// Imaged from there should be taken and put through the infrapix, then opencv Nope static camera view of the braaaains
 	// video2 = NIR pilot cam
 	
+	/**
+	 * future services
+	 */
 	transient public OpenCV IRCamera, camera;
 	transient public Keyboard keyboard;
 	transient public WebGUI webgui;
@@ -28,16 +34,28 @@ public class Plantoid extends Service {
 	transient public AudioFile audioFile;
 		
 	// system specific data
+	/**
+	 * default port of the Arduino mega
+	 */
 	public String port = "/dev/ttyACM0";
-	public int d2Pin = 2;
-	public int d3Pin = 3;
-	public int d4Pin = 4;
-	public int d5Pin = 5;
 	
+	/**
+	 * default pin of legs d2 through d5
+	 */
+	public int leg1Pin = 2;
+	public int leg2Pin = 3;
+	public int leg3Pin = 4;
+	public int leg4Pin = 5;
+	
+	/**
+	 * default pins for pan tilt kit
+	 */
 	public int panPin = 6;
 	public int tiltPin = 7;
 	
-	// analog read pins
+	/**
+	 * analog read pins
+	 */
 	public int soildMoisture = 0;
 	public int tempHumidity = 2;
 	public int leftLight = 4;
@@ -48,6 +66,14 @@ public class Plantoid extends Service {
 
 	public final static Logger log = LoggerFactory.getLogger(Plantoid.class.getCanonicalName());
 
+	/**
+	 * Plantoid Service - this service controls all peer services.  It is a OrbousMundus Genus and
+	 * flagship of the BEPSL Republic.
+	 * 
+	 *  Its mission is to go forth explore and be one with nature in alien environments while reporting
+	 *  telemetry back to BEPSL control
+	 * @param n
+	 */
 	public Plantoid(String n) {
 		super(n, Plantoid.class.getCanonicalName());
 
@@ -66,6 +92,8 @@ public class Plantoid extends Service {
 
 		// reserve("pilotcam","OpenCV", "One of the cameras");
 		// reserve("ircamera","OpenCV", "One of the cameras");
+		
+		// TODO - reserve future services
 
 	}
 
@@ -83,10 +111,10 @@ public class Plantoid extends Service {
 			arduino = (Arduino) startReserved("arduino");
 			arduino.connect(port);
 
-			d2 = (Servo) startReserved("d2");
-			d3 = (Servo) startReserved("d3");
-			d4 = (Servo) startReserved("d4");
-			d5 = (Servo) startReserved("d5");
+			leg1 = (Servo) startReserved("d2");
+			leg2 = (Servo) startReserved("d3");
+			leg3 = (Servo) startReserved("d4");
+			leg4 = (Servo) startReserved("d5");
 
 			
 			pan = (Servo) startReserved("pan");
@@ -94,6 +122,7 @@ public class Plantoid extends Service {
 
 			startPolling();
 			attachServos();
+			detachLegs(); // at the moment detach legs
 			stop();
 			
 		} catch (Exception e) {
@@ -101,6 +130,14 @@ public class Plantoid extends Service {
 		}
 	}
 
+	/**
+	 * Connects the plantoid server's Arduino service to the appropriate serial port.
+	 * This is automatically called when the Plantoid service starts.
+	 * Default is /dev/ttyACM0
+	 * @param port
+	 * @return
+	 * true if connected false otherwise
+	 */
 	public boolean connect(String port) {
 		this.port = port;
 		arduino = (Arduino) startReserved("arduino");
@@ -109,6 +146,12 @@ public class Plantoid extends Service {
 		return true;
 	}
 	
+	/**
+	 * This begins polling of the various analog senesors of the 
+	 * Plantoid server.  It is automatically started when the Plantoid
+	 * service is started. Soil, temperature, left and right light sensors
+	 * and air quality are all polled
+	 */
 	public void startPolling()
 	{
 		arduino.setSampleRate(sampleRate);
@@ -119,6 +162,9 @@ public class Plantoid extends Service {
 		arduino.analogReadPollingStart(airQuality);
 	}
 	
+	/**
+	 * shut down polling of analog sensors
+	 */
 	public void stopPolling(){
 		arduino.analogReadPollingStop(soildMoisture);
 		arduino.analogReadPollingStop(tempHumidity);
@@ -128,30 +174,60 @@ public class Plantoid extends Service {
 	}
 
 	// ------- servos begin -----------
+	
+	/**
+	 * Spin spins the plantoid server
+	 * @param power
+	 * power range is from 
+	 *         -90 (spin full clock wise)
+	 *         0 (stop)
+	 *         90 (spin full counter clockwise)
+	 */
 	public void spin(Integer power) {
 		int s = 90 - power;
-		d3.moveTo(s);
-		d4.moveTo(s);
-		d5.moveTo(s);
-		d2.moveTo(s);
+		leg2.moveTo(s);
+		leg3.moveTo(s);
+		leg4.moveTo(s);
+		leg1.moveTo(s);
 	}
 	
+	/**
+	 * moveY moves the plantoid on the Y axis
+	 * @param power
+	 *         -90 - down the Y axis
+	 *         0 (stop)
+	 *         90 - up the Y axis
+	 */
 	public void moveY(Integer power){
 		int s = 90 - power;
-		d3.moveTo(90);
-		d4.moveTo(s);
-		d5.moveTo(90);
-		d2.moveTo(s);
+		leg2.moveTo(90);
+		leg3.moveTo(s);
+		leg4.moveTo(90);
+		leg1.moveTo(s);
 	}
 
-	public void moveX(Integer power){
+	/**
+	 * moveX moves the plantoid on the X axis
+	 * @param power
+	 *         -90 - down the X axis
+	 *         0 (stop)
+	 *         90 - up the X axis
+	 */	public void moveX(Integer power){
 		int s = 90 - power;
-		d2.moveTo(s);
-		d3.moveTo(90);
-		d4.moveTo(s);
-		d5.moveTo(90);
+		leg1.moveTo(s);
+		leg2.moveTo(90);
+		leg3.moveTo(s);
+		leg4.moveTo(90);
 	}
 	
+	/**
+	 * square dance "should" make a square by moving the plantoid
+	 * up and down the X and Y axis's
+	 * @param power
+	 * power applied to legs
+	 * @param time
+	 * run time on each axis
+	 */
 	public void squareDance(Integer power, Integer time)
 	{
 		int s = 90 - power;
@@ -166,53 +242,91 @@ public class Plantoid extends Service {
 		stop();
 	}
 	
+	/**
+	 * stops all legs
+	 */
 	public void stop() {
-		d2.moveTo(90);
-		d3.moveTo(90);
-		d4.moveTo(90);
-		d5.moveTo(90);
+		leg1.moveTo(90);
+		leg2.moveTo(90);
+		leg3.moveTo(90);
+		leg4.moveTo(90);
 	}
 
+	/**
+	 * attaches all the servos 
+	 * legs and pan tilt kit
+	 */
 	public void attachServos()
 	{
 		attachPanTilt();
 		attachLegs();
 	}
 	
+	/**
+	 * detaches all servos
+	 */
 	public void detachServos()
 	{
 		detachPanTilt();
 		detachLegs();
 	}
 	
+	/**
+	 * attaches only the pan tilt
+	 */
 	public void attachPanTilt() {
 		arduino.servoAttach(pan.getName(), panPin);
 		arduino.servoAttach(tilt.getName(), tiltPin);
 	}	
 	
+	/**
+	 * detaches the pan tilt only
+	 */
 	public void detachPanTilt() {
 		pan.detach();
 		tilt.detach();
 	}	
 	
+	/**
+	 * attaches the legs only
+	 */
 	public void attachLegs() {
-		arduino.servoAttach(d2.getName(), d2Pin);
-		arduino.servoAttach(d3.getName(), d3Pin);
-		arduino.servoAttach(d4.getName(), d4Pin);
-		arduino.servoAttach(d5.getName(), d5Pin);
+		arduino.servoAttach(leg1.getName(), leg1Pin);
+		arduino.servoAttach(leg2.getName(), leg2Pin);
+		arduino.servoAttach(leg3.getName(), leg3Pin);
+		arduino.servoAttach(leg4.getName(), leg4Pin);
 	}
 	
+	/**
+	 * detaches the legs only
+	 */
 	public void detachLegs() {
-		d2.detach();
-		d3.detach();
-		d4.detach();
-		d5.detach();
+		leg1.detach();
+		leg2.detach();
+		leg3.detach();
+		leg4.detach();
 	}
 	// ------- servos begin -----------
 	
+	/**
+	 * shuts down the planoid server
+	 */
 	public void shutdown() {
 		detachServos();
 		Runtime.releaseAll();
+	}
+	
+	/**
+	 * current uptime of the plantoid server
+	 * this represents the longevity and quality
+	 * of our plantoid craft
+	 * LONG LIVE BEPSL !!
+	 * @return
+	 * the uptime
+	 */
+	public String getUptime()
+	{
+		return Runtime.getUptime();
 	}
 
 	public static void main(String[] args) {
