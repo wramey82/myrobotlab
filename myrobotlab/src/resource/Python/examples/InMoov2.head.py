ca59e6fcb2126b34d3a3fcdd2d96c27ce71e9bff
supertick <supertick@gmail.com>
2013-11-24 18:27:38 +0000

    casting fix

--- myrobotlab/src/resource/Python/examples/InMoov2.head.py
+++ myrobotlab/src/resource/Python/examples/InMoov2.head.py
@@ -1,6 +1,6 @@
 headPort = "COM4"
 
-i01 = (InMoov) Runtime.createAndStart("i01", "InMoov")
+i01 = Runtime.createAndStart("i01", "InMoov")
 head = i01.startHead(headPort)
 neck = i01.getHeadTracking()
 neck.faceDetect()
