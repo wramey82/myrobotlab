e0e0dd2e5a4fe2edf3889575b5612304505c2f7b
alessandro.didonna@gmail.com <alessandro.didonna@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2014-01-24 14:38:20 +0000

    InMoov2.Alessandruino.py script added

---
+++ trunk/myrobotlab/src/resource/Python/examples/InMoov2.Alessandruino.py
@@ -0,0 +1,38 @@
+headPort = "COM8"
+
+i01 = Runtime.createAndStart("i01", "InMoov")
+head = i01.startHead(headPort)
+neck = i01.getHeadTracking()
+neck.faceDetect()
+
+#my eyeY servo is reverted, Gael should delete this part
+i01.head.eyeY.setInverted(True)
+i01.head.eyeY.setMinMax(22,85)
+
+
+i01.head.headTracking.xpid.setPID(15.0,5.0,0.1)
+i01.head.headTracking.ypid.setPID(20.0,5.0,0.1)
+i01.head.eyesTracking.xpid.setPID(15.0,5.0,1.0)
+i01.head.eyesTracking.ypid.setPID(15.0,5.0,1.0)
+
+eyes = i01.getEyesTracking()
+eyes.faceDetect()
+
+ear = runtime.createAndStart("ear","Sphinx")
+mouth = runtime.createAndStart("mouth", "Speech")
+ 
+ear.attach(mouth)
+ear.addCommand("attach", "i01.head", "attach")
+ear.addCommand("detach", "i01.head", "detach")
+ear.addCommand("search humans", "i01.head.headTracking", "findFace")
+ear.addCommand("stop searching", "i01.head.headTracking", "stopScan")
+ear.addCommand("rest", "i01.head", "rest")
+ 
+ear.addCommand("manual", ear.getName(), "lockOutAllGrammarExcept", "voice control")
+ear.addCommand("voice control", ear.getName(), "clearLock")
+ 
+ear.addComfirmations("yes", "correct", "yeah", "ya")
+ear.addNegations("no", "wrong", "nope", "nah")
+ 
+# all commands MUST be before startListening
+ear.startListening()
