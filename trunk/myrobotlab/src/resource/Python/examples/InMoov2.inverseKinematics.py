25fa3f5c00a6d6210aa1393779d1d0e0732d0b20
alessandro.didonna@gmail.com <alessandro.didonna@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2014-02-06 11:55:57 +0000

    Added Z axis too... to InMoov2.inverseKinematics.py

--- trunk/myrobotlab/src/resource/Python/examples/InMoov2.inverseKinematics.py
+++ trunk/myrobotlab/src/resource/Python/examples/InMoov2.inverseKinematics.py
@@ -1,7 +1,10 @@
 inversekinematics = Runtime.createAndStart("inversekinematics", "InverseKinematics")
 #insert number of Degrees of Freedom
 dof = 2
-oldDistance = 0
+oldDistance =0
+distance =0
+z = 0
+oldZ = 0
 arduino = Runtime.createAndStart("arduino","Arduino")
 arduino.connect("COM8")
 shoulder	= Runtime.createAndStart("shoulder","Servo")
@@ -28,7 +31,10 @@ opencv.addFilter("PyramidDown1", "PyramidDown")
 opencv.addFilter("FaceDetect1", "FaceDetect")
  
 def input():
- 
+    global oldDistance
+    global distance
+    global oldZ
+    global z
     #print 'found face at (x,y) ', msg_opencv_publishOpenCVData.data[0].x(), msg_opencv_publish.data[0].y()
     opencvData = msg_opencv_publishOpenCVData.data[0]
     if (opencvData.getBoundingBoxArray().size() > 0) :
@@ -40,13 +46,14 @@ def input():
      h = rect.height
      print w
      distance = ((((0.15- w )/0.65)*210)+210)
-     print distance
-     global distance
-     global oldDistance
-
-     if (abs(oldDistance - distance) > 0.2):
+     z = ((-1*(posy*100))+50)
+     print 'z is ' , z
+     print 'distance is ' , distance
+     diffD = abs(oldDistance - distance)
+     diffZ = abs(oldZ - z)
+     if (diffD > 8 or diffZ >5):
          
-      inversekinematics.setPoint(distance,0,0)
+      inversekinematics.setPoint(distance,0,z)
       #start the engine
       inversekinematics.compute()
       #print base angle
