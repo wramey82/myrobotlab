ba218b7a7769519d3473c6547decb32781ef488e
supertick <supertick@gmail.com>
2014-03-27 04:23:26 +0000

    update

--- myrobotlab/src/resource/Python/examples/InMoov2.pir.py
+++ myrobotlab/src/resource/Python/examples/InMoov2.pir.py
@@ -1,20 +1,21 @@
-port = "COM5"
+port = "COM4"
 
 i01 = Runtime.createAndStart("i01", "InMoov")
 mouth = i01.startMouth()
-head = i01.startHead(port)
-arduino = head.arduino
+#head = i01.startHead(port)
+arduino = Runtime.createAndStart("i01.right", "Arduino")
+arduino.connect(port)
 arduino.addListener("publishPin", python.getName(), "input")
 arduino.setSampleRate(8000)
 arduino.digitalReadPollStart(12)
-head.setSpeed(0.5, 0.5, 0.5, 0.5, 0.5)
+#head.setSpeed(0.5, 0.5, 0.5, 0.5, 0.5)
 
 def input():
     # print 'python object is ', msg_clock_pulse
-    pin = msg_i01_left_publishPin.data[0]
+    pin = msg_i01_right_publishPin.data[0]
     print 'pin data is ', pin.pin, pin.value
     if (pin.value == 1):
       mouth.speak("howdy partner")
-      head.neck.moveTo(85)
-      sleep(2)
-      head.neck.moveTo(90)
+      #head.neck.moveTo(85)
+      #sleep(2)
+      #head.neck.moveTo(90)
