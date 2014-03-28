3628f1f80bbafc33ac167898a82787b0bd436890
supertick@gmail.com <supertick@gmail.com@8fea13ce-8e12-efca-9acd-5addbe237fc1>
2014-03-28 20:24:26 +0000

    user callbacks added

--- trunk/myrobotlab/src/org/myrobotlab/openni/PApplet.java
+++ trunk/myrobotlab/src/org/myrobotlab/openni/PApplet.java
@@ -1,14 +1,21 @@
 package org.myrobotlab.openni;
 
 import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.service.GestureRecognition;
 import org.slf4j.Logger;
 
 import SimpleOpenNI.SimpleOpenNI;
 
 public class PApplet {
+	// FIXME - EXTRACT INTERFACE ----
+	private GestureRecognition gr = null;
 
 	public Object g;
 	public final static Logger log = LoggerFactory.getLogger(PApplet.class);
+	
+	public PApplet(GestureRecognition gr) {
+		this.gr = gr;
+	}
 
 	public String dataPath(String recordPath) {
 		log.info("dataPath");
@@ -29,6 +36,30 @@ public class PApplet {
 
 	static public final float sqrt(float a) {
 		return (float) Math.sqrt(a);
+	}
+	
+	// FIXME - EXTRACT USER INTERFACE BEGIN ----
+	public void onNewUser(SimpleOpenNI openni, int userId){
+		gr.onNewUser(openni, userId);
+	}
+	
+	public void onLostUser(SimpleOpenNI openni, int userId){
+		gr.onLostUser(openni, userId);
+	}
+	
+	public void onOutOfSceneUser(SimpleOpenNI openni, int userId){
+		
+	}
+
+	public void onNewHand(SimpleOpenNI openni, int userId, PVector v){
+		log.info("here");
+		//gr.onNewHand(openni, userId);
+		//gr.on
+	}
+	public void onTrackedHand(SimpleOpenNI openni, int userId){
+		log.info("here");
+		gr.onNewUser(openni, userId);
+		//gr.on
 	}
 
 }
